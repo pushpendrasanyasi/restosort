677ccb14aa8da88c92abde54c2fc9a830d46ec23
Pushpendra <pushpendra3010@gmail.com>
2023-02-19 14:07:25 +0530

    blacklist target and idea directory in gitignore add 100 recommendation limit

---
+++ src/main/java/com/solution/common/constants/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.solution.common.constants;
+
+public class ApplicationConstants {
+
+    public static final int MAX_RECOMMENDATION_COUNT = 100;
+
+}
--- src/main/java/com/solution/services/RecommendationRuleProcessor.java
+++ src/main/java/com/solution/services/RecommendationRuleProcessor.java
@@ -1,5 +1,6 @@
 package com.solution.services;
 
+import com.solution.common.constants.ApplicationConstants;
 import com.solution.entities.Restaurant;
 import com.solution.entities.UserMeta;
 import com.solution.services.rules.booleanrule.BooleanRecommendationRule;
@@ -10,10 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -39,11 +37,11 @@ public class RecommendationRuleProcessor {
             List<Integer> matchedRestIds = rule.getMatchingRestaurants(userMeta, remainingRestaurants);
             out.addAll(matchedRestIds);
             remainingRestaurants = remainingRestaurants(matchedRestIds, remainingRestaurants);
-            if (CollectionUtils.isEmpty(remainingRestaurants)) {
+            if (CollectionUtils.isEmpty(remainingRestaurants) || out.size() >= ApplicationConstants.MAX_RECOMMENDATION_COUNT) {
                 break;
             }
         }
-        return out;
+        return truncateExtraRecommendations(out);
     }
 
     private List<Restaurant> remainingRestaurants(List<Integer> recommendations, List<Restaurant> restaurants) {
@@ -51,5 +49,15 @@ public class RecommendationRuleProcessor {
         return restaurants.stream()
                 .filter(restaurant -> !recommendationSet.contains(restaurant.getRestaurantId()))
                 .collect(Collectors.toList());
+    }
+
+    private List<Integer> truncateExtraRecommendations(List<Integer> out) {
+        if (CollectionUtils.isEmpty(out)) {
+            return Collections.emptyList();
+        }
+        if (out.size() >= ApplicationConstants.MAX_RECOMMENDATION_COUNT) {
+            return out.subList(0, ApplicationConstants.MAX_RECOMMENDATION_COUNT);
+        }
+        return out;
     }
 }
